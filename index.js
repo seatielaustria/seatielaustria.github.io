c92e3f52da6f2767463e6aa76d9277d22511a9c4
Seatiel <seatiel.austria@boom.camp>
2022-02-06 20:05:39 +0800

    install moment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello World")
+var moment = require('moment')
+
+const curr_year = moment().format('YYYY')
+
+var copyright = document.getElementById("copyright")
+copyright.innerHTML = `Copyright &copy; SexyGirl ${curr_year}`
